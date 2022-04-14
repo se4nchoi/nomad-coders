0a517aab0d2fd59a6cc776a360eb7df62ba8c7ec
Sean <yhchoi@emgglobal.co.kr>
2022-04-14 23:30:21 +0900

    [nextjs-intro] Add section 2.4 files "

--- nextjs-intro/next.config.js
+++ nextjs-intro/next.config.js
@@ -1,6 +1,33 @@
 /** @type {import('next').NextConfig} */
+
+// const API_KEY = "4a638e3d45b958f594e3004335e6d5ed"
+// to get rid of this from ur build, make .env (gitignore it)
+const API_KEY = process.env.API_KEY;
+
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-sexy-blog/:path*",
+        permanent: false,
+      },
+      {
+        source: "/hehe/:path*",
+        destination: "/haha/:path*",
+        permanent: false,
+      }
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,29 +1,58 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-const API_KEY = "4a638e3d45b958f594e3004335e6d5ed"
-
-export default function Home() {
+export default function Home({ results }) {
     const [movies, setMovies] = useState();
-    useEffect(() => {
-        (async () => {
-            const response = await fetch(
-                `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-            )
-            const {results} = await response.json();
-            setMovies(results);
-        })();
-    }, []);
-        
+    
     return (
-    <div>
+    <div className="container">
         <Seo title="Home"/>
-        {!movies && <h4>Loading...</h4>}
-        {movies?.map(movie => (
-            <div key={movie.id}>
+        {results?.map(movie => (
+            <div key={movie.id} className="movie">
+                <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}/>
                 <h4>{movie.original_title}</h4>
             </div>
         ))}
+
+        <style jsx>{`
+            .container {
+                display: grid;
+                grid-template-columns: 1fr 1fr;
+                padding: 1.25rem;
+                gap: 1.25rem;
+            }
+            .movie img {
+                max-width: 100%;
+                border-radius: 12px;
+                transition: transform 0.2s ease-in-out;
+                box-shadow: rgba(0,0,0, 0.1) 0px 4px 12px;
+            }
+            .movie {
+                cursor: pointer;
+            }
+            .movie:hover img {
+                transform: scale(1.05) translateY(-4px);
+            }
+            .movie h4 {
+                font-size: 1.25rem;
+                text-align: center;
+            }
+        `}</style>
     </div>
     );
+}
+
+export async function getServerSideProps() {
+    // here is where you could also put API keys, etc.
+    // only visible on the server side !
+
+    const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+    // ERROR: absolute url; cannot just keep the url as /api/movies, 
+    // as the server does not know this url.
+
+    return {
+        props: {
+            results,
+        }
+    }
 }
